2643becfb0cf3fda4dd17fca863b24e91f8ec8ab
German <g44ravarotto@gmail.com>
2024-04-02 18:52:45 -0300

    Style(frontend):Add fonts Dm sans

--- frontend-nocountry-s14-17-t-java/tailwind.config.js
+++ frontend-nocountry-s14-17-t-java/tailwind.config.js
@@ -3,7 +3,11 @@ import daisyui from "daisyui";
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        "dm-sans": ['"DM Sans"', "sans-serif"],
+      },
+    },
   },
   plugins: [daisyui],
   daisyui: {
