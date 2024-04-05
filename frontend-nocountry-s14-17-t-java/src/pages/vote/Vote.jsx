46e7010c76909a456c6ddbaed340779ad02670ac
FrancoAmicone <francoamicone1@gmail.com>
2024-04-05 13:55:37 -0300

    style(votecard) primmer commit

--- frontend-nocountry-s14-17-t-java/src/pages/vote/Vote.jsx
+++ frontend-nocountry-s14-17-t-java/src/pages/vote/Vote.jsx
@@ -7,12 +7,24 @@ const voting = "Rodrigo"
 
 
 const Vote = () => {
+  const preguntas = ["¿Se sienten seguros y apoyados por sus compañeros?"]
+
+
   return (
 
 <div className="w-full h-screen flex items-center justify-center bg-[#06071B]">
   <TittleGradient user={user} voting={voting}/>
+  <div className="w-1200 h-350 rounded-3xl		 bg-gradient-to-b from-blue-400 to-#06071B  opacity-30    p-20         flex-col flex mt-40 ">
+  {preguntas.map((pregunta, index) => (
+        <div key={index} className=''>
+          
+          <h3 className=" text-2xl text-center font-medium mb-2 text-white " >Pregunta {index + 1}:</h3>
+          <h1 className="text-3xl text-center font-bold text-white " >{pregunta}</h1>
+        </div>
+      ))}
+  </div>
 
-  <h2 className='text-3xl'>Pregunta 1.</h2>
+
 
 
 
