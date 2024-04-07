1559425b542895f58505ef07d29cc4a12101c767
German <g44ravarotto@gmail.com>
2024-04-07 19:49:12 -0300

    fix menu

--- frontend-nocountry-s14-17-t-java/src/components/DashBoardComponents/DashBoardGraphic.jsx
+++ frontend-nocountry-s14-17-t-java/src/components/DashBoardComponents/DashBoardGraphic.jsx
@@ -3,7 +3,7 @@ const DashBoardGraphic = () => {
     <div className="w-full h-full bg-black border-[1px] border-[#09FCA7] rounded-[10px] flex flex-col items-center justify-around">
       <h3 className="text-warning text-2xl font-bold">Votacion total</h3>
       <div
-        className="daisy-radial-progress text-warning text-xl font-bold border-4 border-warning"
+        className="daisy-radial-progress text-warning text-xl font-bold "
         style={{ "--value": "70", "--size": "8rem", "--thickness": "10px" }}
         role="progressbar"
       >
--- frontend-nocountry-s14-17-t-java/src/components/HeaderDash/HeaderDash.jsx
+++ frontend-nocountry-s14-17-t-java/src/components/HeaderDash/HeaderDash.jsx
@@ -1,6 +1,5 @@
 import Logo from "../Logo/Logo";
 import ButtonNeon from "../ButtonNeon/ButtonNeon";
-import DashBoardNavbar from "../DashBoardComponents/DashBoardNavbar";
 
 const HeaderHome = () => {
   return (
@@ -8,7 +7,6 @@ const HeaderHome = () => {
       <div className=" w-full h-[58px] flex justify-around items-center bg-black">
         <Logo />
         <div className="w-auto h-auto flex items-center gap-2">
-          <DashBoardNavbar />
           <ButtonNeon text="Log Out" />
         </div>
       </div>
