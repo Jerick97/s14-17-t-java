a4c81bf911bd838d670b02cca2657f0846913908
fr3nm0 <efren.morales.flores@gmail.com>
2024-04-09 22:15:38 -0600

    [ADD]: Endpoints basicos para la entidad proyectos

---
+++ backend-nocountry-s14-17-t-java/src/main/java/com/nocountry/TeamScore/projects/model/dto/ProjectRequest.java
@@ -0,0 +1,22 @@
+package com.nocountry.TeamScore.projects.model.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class ProjectRequest {
+
+    private String name;
+    private String description;
+    private LocalDate publishDate;
+    private LocalDateTime startsOn;
+    private LocalDateTime endsOn;
+}
