efc110b9561cddf22ae655045c5cdddf055eb586
sturniolo <br1.sturniolo@gmail.com>
2024-04-11 12:13:59 -0300

    [ADD]services en feedback

--- backend-nocountry-s14-17-t-java/src/main/java/com/nocountry/TeamScore/feedback/controllers/FeedbackController.java
+++ backend-nocountry-s14-17-t-java/src/main/java/com/nocountry/TeamScore/feedback/controllers/FeedbackController.java
@@ -43,6 +43,36 @@ public class FeedbackController {
         return ResponseEntity.ok(feedbackService.getAll());
     }
 
+    @SecurityRequirement(name = "bearerAuth")
+    @GetMapping("/hechosPorUser/{username}")
+    public ResponseEntity<?> feedbacksHechosPorUser(@PathVariable String username) {
+        return ResponseEntity.ok(feedbackService.feedbacksHechosPorUser(username));
+    }
+
+    @SecurityRequirement(name = "bearerAuth")
+    @GetMapping("/hechosParaElUser/{username}")
+    public ResponseEntity<?> feedbacksHechosParaElUser(@PathVariable String username) {
+        return ResponseEntity.ok(feedbackService.feedbacksHechosParaElUser(username));
+    }
+
+    @SecurityRequirement(name = "bearerAuth")
+    @GetMapping("/porGrupoAUnUsuario/{idGrupo}/{usuarioEvaluado}")
+    public ResponseEntity<?> feedbacksPorGrupoAUnUsuario(@PathVariable Long idGrupo, @PathVariable String usuarioEvaluado) {
+        return ResponseEntity.ok(feedbackService.feedbacksPorGrupoAUnUsuario(idGrupo, usuarioEvaluado));
+    }
+
+    @SecurityRequirement(name = "bearerAuth")
+    @GetMapping("/hechosEnElGrupoPorElUsuario/{idGrupo}/{usuarioQEvalua}")
+    public ResponseEntity<?> feedbacksHechosEnElGrupoPorElUsuario(@PathVariable Long idGrupo, @PathVariable String usuarioQEvalua) {
+        return ResponseEntity.ok(feedbackService.feedbacksHechosEnElGrupoPorElUsuario(idGrupo, usuarioQEvalua));
+    }
+
+    @SecurityRequirement(name = "bearerAuth")
+    @GetMapping("/hechosPorElUsuarioParaElUsuario/{UsuarioQEvalua}/{usuarioEvaluado}/{idGrupo}")
+    public ResponseEntity<?> feedbacksHechosPorElUsuarioParaElUsuario(@PathVariable String UsuarioQEvalua, @PathVariable String usuarioEvaluado, @PathVariable Long idGrupo) {
+        return ResponseEntity.ok(feedbackService.feedbacksHechosPorElUsuarioParaElUsuario(UsuarioQEvalua, usuarioEvaluado, idGrupo));
+    }
+
     // faltarian enpoints para recuperar los feedback echos en un proyecto, o por un usuario
     // endpoint para recuperar el rol del usuario al que estoy evaluando
     // considerar responsabilidad del feedback saber cuantos feedback por proyecto hay y cuantos faltan completar en el grupo
