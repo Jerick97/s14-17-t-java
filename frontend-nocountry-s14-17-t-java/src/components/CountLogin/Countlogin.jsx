3b65c48fa4c4bd05be89609a5d4bfbf9d4bd70c0
Alejo <alejohurtadotesta@gmail.com>
2024-04-11 18:32:06 -0300

    Creacion del componente contador de dias restantes y se importa en el login. Se modifica algunos estilos del login.

---
+++ frontend-nocountry-s14-17-t-java/src/components/CountLogin/Countlogin.jsx
@@ -0,0 +1,50 @@
+import { useEffect } from "react";
+import { useState } from "react";
+
+function Count() {
+  const [startDate] = useState(new Date("2024-04-10"));
+  const [endDate] = useState(new Date("2024-05-10"));
+  const [currentDate, setCurrentDate] = useState(new Date());
+
+  /* ACTUALIZAMOS LA FECHA ACTUAL (CURRENTDATE) CADA SEGUNDO SETINTERVAL( () => {}, 1000 ) */
+  useEffect(() => {
+    /* LA FUNCION SETINTERVAL SE EJECUTA CADA SEGUNDO, ACTUALIZANDO EL ESTADO DE CURRENTDATE CON LA FECHA ACTUAL */
+    const interval = setInterval(() => {
+      setCurrentDate(new Date());
+    }, 1000);
+
+    /* LA FUNCION QUE RETORNAMOS SE EJECUTA AL DESMONTAR EL COMPONENTE, POR ESO LIMPIAMOS EL INTERVALO PARA LIBERAR LA MEMORIA */
+    return () => clearInterval(interval);
+  }, []);
+
+  const calculateDaysRemaining = () => {
+    /* UTILIZAMOS "getTime()" PARA OBTENER EL TIEMPO EN MILISEGUNDOS (desde 1/1/70) DE AMBAS FECHAS Y LAS RESTAMOS, NOS DA LA DIFERENCIA ENTRE AMBAS FECHAS */
+    const daysDifference = endDate.getTime() - currentDate.getTime();
+    /* HACEMOS LA CONVERCION DE MILISEGUNDOS A DIAS, DIVIDIMOS LA DIFERENCIA POR LOS MILISEGUNDOS EN UN DIA Y OBTENEMOS LOS DIAS RESTANTES. UTILIZAMOS "Math.ceil()" PARA REDONDEAR PARA ARRIBA Y DARNOS UN NUMERO ENTERO EN DIAS. */
+    const daysRemaining = Math.ceil(daysDifference / (1000 * 3600 * 24));
+    return daysRemaining;
+  };
+
+  const daysRemainingDate = calculateDaysRemaining();
+
+  /* RENDERIZAMOS CON ".toLocaleDateString()" PARA TRANSFORMAR LAS FECHAS EN CADENAS DE TEXTO Y DE ACUERDO A LA REGION EN LA QUE NOS ENCONTREMOS */
+  return (
+    <div>
+      <h1>
+        Fecha de inicio de la calificacion a compañeros:{" "}
+        <span style={{ color: "#1d90fc" }}>
+          {startDate.toLocaleDateString()}
+        </span>
+        . <br />
+        Faltan <span style={{ color: "#1d90fc" }}>
+          {daysRemainingDate}
+        </span>{" "}
+        dias para que finalice el periodo de calificacion. <br />
+        Fecha de finalizacion de la calificacion:{" "}
+        <span style={{ color: "#1d90fc" }}>{endDate.toLocaleDateString()}</span>
+      </h1>
+    </div>
+  );
+}
+
+export default Count;
--- frontend-nocountry-s14-17-t-java/src/pages/auth/login/Login.jsx
+++ frontend-nocountry-s14-17-t-java/src/pages/auth/login/Login.jsx
@@ -4,6 +4,7 @@ import LogoImg from "../../../assets/LogoNoCountry.svg";
 import UserInput from "../../../components/UserInput/UserInput";
 import PasswordInput from "../../../components/PasswordInput/PasswordInput";
 import ButtonPrimary from "../../../components/ButtonPrimary/ButtonPrimary";
+import Count from "../../../components/CountLogin/Countlogin";
 
 function Login() {
   const navigate = useNavigate();
@@ -44,82 +45,87 @@ function Login() {
           "radial-gradient(circle farthest-corner at 50% 0%, rgba(29, 144, 252, .29), #06071b)",
       }}
     >
-      <div className="container mx-auto px-4 sm:px-6 lg:px-8 max-w-screen-lg h-full flex items-center justify-center md:flex-row flex-col">
-        <div className="w-full flex items-center justify-center flex-col">
-          <img src={LogoImg} alt="logo" className="w-80" />
-          <h1 className="my-4 md:text-6xl text-3xl text-white font-medium text-center">
-            Feedback{" "}
-            <span className="bg-gradient-to-r from-[#1d90fc] to-[#0cfca7] inline-block text-transparent bg-clip-text">
-              a tus compañeros
-            </span>
-          </h1>
+      <div className="container mx-auto px-4 sm:px-6 lg:px-8 max-w-screen-lg h-full flex items-center justify-center flex-row flex-col">
+        <div className="container flex">
+          <div className="w-full flex items-center justify-center flex-col">
+            <img src={LogoImg} alt="logo" className="w-80" />
+            <h1 className="my-4 md:text-6xl text-3xl text-white font-medium text-center">
+              Feedback{" "}
+              <span className="bg-gradient-to-r from-[#1d90fc] to-[#0cfca7] inline-block text-transparent bg-clip-text">
+                a tus compañeros
+              </span>
+            </h1>
+          </div>
+          <div className="h-96 w-full max-w-sm relative overflow-hidden mt-6 z-10 rounded-2xl">
+            <div className="absolute inset-0 bg-gradient-to-br from-[#091A3A] from-[50%] to-[#0C274F] to-[50%] transform skew-y-45"></div>
+            <form
+              className="absolute inset-0 flex justify-around items-center flex-col sm:p-8 p-4"
+              autoComplete="off"
+              onSubmit={handleSubmit(handleLogin)}
+            >
+              <h4 className="text-white text-2xl text-center font-extrabold uppercase mb-5">
+                User Login
+              </h4>
+              <div className="my-2 w-full">
+                <div className="mb-4">
+                  <UserInput
+                    {...register("email", {
+                      required: {
+                        value: true,
+                        message: "Email is required",
+                      },
+                      pattern: {
+                        value: /^[\w-ñÑ]+(\.[\w-ñÑ]+)*@[\w-ñÑ]+(\.[\w-ñÑ]+)+$/,
+                        message: "Please enter a valid email",
+                      },
+                    })}
+                    name="email"
+                    placeholder="Email"
+                    customStyle={
+                      errors.email
+                        ? "border-2 border-solid border-red-500 placeholder:text-red-400 text-red-400"
+                        : "text-[#acacb5]"
+                    }
+                  />
+                  {errors.email && (
+                    <p className={`ml-12 text-red-500 text-sm`}>
+                      {errors.email?.message}
+                    </p>
+                  )}
+                </div>
+                <div className="mb-4">
+                  <PasswordInput
+                    {...register("password", {
+                      required: {
+                        value: true,
+                        message: "Password is required",
+                      },
+                      minLength: {
+                        value: 8,
+                        message: "Minimum 8 characters",
+                      },
+                    })}
+                    name="password"
+                    placeholder="Password"
+                    customStyle={
+                      errors.password
+                        ? "border-2 border-solid border-red-500 placeholder:text-red-400 text-red-400"
+                        : "text-[#acacb5]"
+                    }
+                  />
+                  {errors.password && (
+                    <p className={`ml-12 text-red-500 text-sm`}>
+                      {errors.password?.message}
+                    </p>
+                  )}
+                </div>
+              </div>
+              <ButtonPrimary text="Login" type="submit" />
+            </form>
+          </div>
         </div>
-        <div className="h-96 w-full max-w-sm relative overflow-hidden mt-6 z-10 rounded-2xl">
-          <div className="absolute inset-0 bg-gradient-to-br from-[#091A3A] from-[50%] to-[#0C274F] to-[50%] transform skew-y-45"></div>
-          <form
-            className="absolute inset-0 flex justify-around items-center flex-col sm:p-8 p-4"
-            autoComplete="off"
-            onSubmit={handleSubmit(handleLogin)}
-          >
-            <h4 className="text-white text-2xl text-center font-extrabold uppercase mb-5">
-              User Login
-            </h4>
-            <div className="my-2 w-full">
-              <div className="mb-4">
-                <UserInput
-                  {...register("email", {
-                    required: {
-                      value: true,
-                      message: "Email is required",
-                    },
-                    pattern: {
-                      value: /^[\w-ñÑ]+(\.[\w-ñÑ]+)*@[\w-ñÑ]+(\.[\w-ñÑ]+)+$/,
-                      message: "Please enter a valid email",
-                    },
-                  })}
-                  name="email"
-                  placeholder="Email"
-                  customStyle={
-                    errors.email
-                      ? "border-2 border-solid border-red-500 placeholder:text-red-400 text-red-400"
-                      : "text-[#acacb5]"
-                  }
-                />
-                {errors.email && (
-                  <p className={`ml-12 text-red-500 text-sm`}>
-                    {errors.email?.message}
-                  </p>
-                )}
-              </div>
-              <div className="mb-4">
-                <PasswordInput
-                  {...register("password", {
-                    required: {
-                      value: true,
-                      message: "Password is required",
-                    },
-                    minLength: {
-                      value: 8,
-                      message: "Minimum 8 characters",
-                    },
-                  })}
-                  name="password"
-                  placeholder="Password"
-                  customStyle={
-                    errors.password
-                      ? "border-2 border-solid border-red-500 placeholder:text-red-400 text-red-400"
-                      : "text-[#acacb5]"
-                  }
-                />
-                {errors.password && (
-                  <p className={`ml-12 text-red-500 text-sm`}>
-                    {errors.password?.message}
-                  </p>
-                )}
-              </div>
-            </div>
-            <ButtonPrimary text="Login" type="submit" />
-          </form>
+        <div className="m-10 text-center text-lg">
+          <Count />
         </div>
       </div>
     </div>
