3ced5e46596b699c7b9874ac035e8ab8bc280d69
sturniolo <br1.sturniolo@gmail.com>
2024-04-13 12:07:30 -0300

    [FIX]create empty group, & [comments]en el controller de group

--- backend-nocountry-s14-17-t-java/src/main/java/com/nocountry/TeamScore/groups/model/dto/GroupDTO.java
+++ backend-nocountry-s14-17-t-java/src/main/java/com/nocountry/TeamScore/groups/model/dto/GroupDTO.java
@@ -1,10 +1,10 @@
 package com.nocountry.TeamScore.groups.model.dto;
 
+import com.nocountry.TeamScore.projects.model.Project;
 import lombok.Data;
 
 @Data
-public class GroupDTO {
-    private Long projectId; // esto es una FK pero de momento la dejo en Long hasta crear su correspondiente entidad
+public class GroupDTO { // para crear grupos vacios
     private String name;
     private String description;
     private Integer status;
