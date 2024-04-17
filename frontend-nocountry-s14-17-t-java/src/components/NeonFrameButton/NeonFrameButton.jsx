a5eb4d95d9a906443ab5d87b370a675e3ddffe12
Emerson Suarez <emersonsuarez2904@gmail.com>
2024-04-17 18:37:53 -0500

    feat(frontend): Se agrega pantalla para Seleccionar tu grupo

---
+++ frontend-nocountry-s14-17-t-java/src/components/NeonFrameButton/NeonFrameButton.jsx
@@ -0,0 +1,27 @@
+import PropTypes from "prop-types";
+
+const NeonFrameButton = ({ id, text, onClick, disabled }) => {
+  const disabledClass = "border-[#A8A8A8] text-[#A8A8A8] cursor-not-allowed";
+  return (
+    <div className="md:col-span-4 sm:col-span-6 col-span-12">
+      <button
+        onClick={() => onClick({ id, text })}
+        className={`w-full h-20 text-xl border-solid border-2 border-[#09FCA7] bg-transparent rounded-[5px] px-[20px] py-[8px] text-[#09FCA7] no-underline ${
+          disabled ? disabledClass : "hover:shadow-[0px_0px_10px_0px_#09FCA7]"
+        }`}
+        disabled={disabled}
+      >
+        {text}
+      </button>
+    </div>
+  );
+};
+
+NeonFrameButton.propTypes = {
+  id: PropTypes.string.isRequired,
+  text: PropTypes.string.isRequired,
+  onClick: PropTypes.func.isRequired,
+  disabled: PropTypes.bool,
+};
+
+export default NeonFrameButton;
--- frontend-nocountry-s14-17-t-java/src/pages/main/Home.jsx
+++ frontend-nocountry-s14-17-t-java/src/pages/main/Home.jsx
@@ -1,7 +1,8 @@
-import { useContext, useEffect, useState } from "react";
+import { useContext } from "react";
 import HeaderHome from "../../components/HeaderHome/HeaderHome";
 import Partners from "../../components/Partners/Partners";
 import { AuthContext } from "../../context/AuthContext";
+import UserGroups from "../../components/UserGroups/UserGroups";
 
 export default function Home() {
   return <HomeContent />;
@@ -9,50 +10,76 @@ export default function Home() {
 
 function HomeContent() {
   const { users } = useContext(AuthContext);
-  // Usar los datos iniciales si están disponibles, de lo contrario, inicializar como un array vacío
 
   const user = "Camilo";
-
+  const groups = [
+    { id: "1", name: "S14-17-t-Java" },
+    /*{ id: "2", name: "S14-Java-tt" },
+    { id: "3", name: "S14-Node-tt" }, */
+    //Si existen más de 1 grupo te pedira seleccionar tu grupo
+  ];
   const resetData = () => {
     localStorage.removeItem("users");
     window.location.reload();
   };
 
   return (
-    <div className='w-full bg-[#06071B] min-h-screen text-white'>
+    <div className="w-full bg-[#06071B] min-h-screen text-white">
       <HeaderHome />
-      <div className='p-10'>
-        <div className='flex items-center'>
-          <div>
-            <h1 className='text-2xl font-bold text-white'>
-              Bienvenido {user} a
-            </h1>
-            <span className='bg-gradient-to-r font-extrabold text-3xl from-[#1d90fc] to-[#0cfca7] inline-block text-transparent bg-clip-text'>
-              Team Score
-            </span>
-            <h3>Tus compañeros de cohorte son:</h3>
+      {/** Si existe más de 1 grupo */}
+      {groups && groups.length > 1 ? (
+        <UserGroups name={user} groups={groups} />
+      ) : groups && groups.length === 1 ? (
+        <div className="p-10">
+          <div className="flex items-center">
+            <div>
+              <h1 className="text-2xl font-bold text-white">
+                Bienvenido {user} a
+              </h1>
+              <span className="bg-gradient-to-r font-extrabold text-3xl from-[#1d90fc] to-[#0cfca7] inline-block text-transparent bg-clip-text">
+                Team Score
+              </span>
+              <h3>Tus compañeros de cohorte son:</h3>
+            </div>
+            <button
+              onClick={resetData}
+              className="bg-gray-600 ml-32 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+            >
+              Restablecer Datos
+            </button>
           </div>
-          <button
-            onClick={resetData}
-            className='bg-gray-600 ml-32 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline'
+          <div className="flex flex-wrap gap-x-10 gap-y-5 mt-5">
+            {users.map((user, index) => (
+              <Partners
+                key={index}
+                name={user.nombres}
+                surname={user.apellidos}
+                role={user.rol}
+                staff={user.staff}
+                index={index}
+                activo={user.state}
+              />
+            ))}
+          </div>
+        </div>
+      ) : (
+        <div className="p-10 w-full h-96 flex flex-col items-center justify-center">
+          <h3 className="text-white text-xl font-bold text-center">
+            No estás asignado a ningún grupo en este momento.
+          </h3>
+          <h3 className="text-white text-lg text-center">
+            {" "}
+            Por favor, ponte en contacto con el soporte técnico para obtener
+            asistencia:
+          </h3>
+          <a
+            href="mailto:contacto@nocountry.io"
+            className="text-blue-300 hover:text-blue-400 block"
           >
-            Restablecer Datos
-          </button>
+            contacto@nocountry.io
+          </a>
         </div>
-        <div className='flex flex-wrap gap-x-10 gap-y-5 mt-5'>
-          {users.map((user, index) => (
-            <Partners
-              key={index}
-              name={user.nombres}
-              surname={user.apellidos}
-              role={user.rol}
-              staff={user.staff}
-              index={index}
-              activo={user.state}
-            />
-          ))}
-        </div>
-      </div>
+      )}
     </div>
   );
 }
