847e6ee10fc50f669b2c2fbf68105c8332d3bbaa
German <g44ravarotto@gmail.com>
2024-04-17 17:49:51 -0300

    fix value GraficProgress

--- frontend-nocountry-s14-17-t-java/src/components/DashBoardComponents/DashBoardGraphic.jsx
+++ frontend-nocountry-s14-17-t-java/src/components/DashBoardComponents/DashBoardGraphic.jsx
@@ -5,20 +5,21 @@ const DashBoardGraphic = ({ users }) => {
   // Calcular el porcentaje
   const totalUsers = users.length;
   const percentageProgressZero = (countProgressZero / totalUsers) * 100;
+  const percentageProgressOne = 100 - percentageProgressZero;
 
   return (
-    <div className="w-full h-full bg-black border-[1px] border-[#09FCA7] rounded-[10px] flex flex-col items-center justify-around">
+    <div className="w-full h-full bg-black border-[1px] border-[#09FCA7] rounded-[10px] flex flex-col items-center justify-around p-2">
       <h3 className="text-warning text-2xl font-bold">Votación total</h3>
       <div
         className="daisy-radial-progress text-warning text-xl font-bold"
         style={{
-          "--value": `${percentageProgressZero}`,
-          "--size": "8rem",
-          "--thickness": "10px",
+          "--value": `${percentageProgressOne}`,
+          "--size": "7rem",
+          "--thickness": "7px",
         }}
         role="progressbar"
       >
-        {percentageProgressZero}%
+        {percentageProgressOne}%
       </div>
     </div>
   );
